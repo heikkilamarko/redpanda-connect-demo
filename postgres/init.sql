40e21b527ad2edfebf90c07b19341ea5fe9e8540
Marko Heikkilä <heikkila_marko@hotmail.com>
2024-02-01 17:53:41 +0200

    Add http2postgres pipeline.

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,1 +1,10 @@
 CREATE DATABASE grafana;
+CREATE DATABASE benthos;
+
+\c benthos;
+
+CREATE TABLE task (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(200) NOT NULL,
+    created_at timestamptz NOT NULL
+);
