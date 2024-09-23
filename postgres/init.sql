17148b3a2f253cdffbf911a9e35048144032778b
Marko Heikkilä <heikkila_marko@hotmail.com>
2024-09-23 13:21:41 +0300

    Add http-azure pipeline.

--- postgres/init.sql
+++ postgres/init.sql
@@ -3,11 +3,30 @@ CREATE DATABASE redpanda_connect;
 
 \c redpanda_connect;
 
-CREATE TABLE task (
-    id SERIAL NOT NULL,
-    name TEXT NOT NULL,
-    description TEXT,
-    created_at timestamptz NOT NULL
+CREATE TABLE factories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    location VARCHAR(100),
+    production_capacity NUMERIC
 );
 
-SELECT create_hypertable('task', by_range('created_at'));
+INSERT INTO factories (name, location, production_capacity)
+VALUES 
+    ('Factory A', 'New York', 100000),
+    ('Factory B', 'Los Angeles', 150000),
+    ('Factory C', 'Chicago', 120000),
+    ('Factory D', 'Houston', 130000),
+    ('Factory E', 'Phoenix', 110000);
+
+CREATE OR REPLACE FUNCTION get_factory_model()
+RETURNS JSONB AS $$
+BEGIN
+    RETURN (
+        SELECT jsonb_agg(f)
+        FROM (
+            SELECT id, name, location, production_capacity 
+            FROM factories
+        ) f
+    );
+END;
+$$ LANGUAGE plpgsql;
